21d0c5795e74189b80fb8f257c2ba31d170a913e
beckie <beckiechebet6@gmail.com>
2019-10-11 17:41:39 +0300

    made changes to coursework

--- assignment2.js
+++ assignment2.js
@@ -1,3 +1,13 @@
+// Assignment 2
+/*
+   -  write a function thats takes two test marks i.e  test1, and test2
+   -  when it gets the marks, it should check for the best mark from the two and return it
+   -  create another function for coursework mark requiring one parameter  -  a coursework mark, check it
+      then get average of coursework and return current coursework value
+   -  write the final function that takes a single parameter, computes the mark exam , as well as final Exam        mark.
+
+    what is the exam mark of the student ?
+*/
 
 //function that aks someone two marks and returns best mark of the two
 function testMarks(test1,test2) {
@@ -8,8 +18,10 @@ function testMarks(test1,test2) {
 
 //function to get coursework mark
 function courseworkMark(coursework) {
-    let courseworkCurrentMark = testMarks(70 , 80) + coursework / 2;
-    let curentcourseworkMark =  courseworkCurrentMark * (40 / 100);
+    let courseworkCurrentMark = testMarks(70 , 80) + coursework;
+    let averageCourseWorkMark = courseworkCurrentMark / 2;
+
+    let curentcourseworkMark =  averageCourseWorkMark * (40 / 100);
 
     return curentcourseworkMark;
 }
