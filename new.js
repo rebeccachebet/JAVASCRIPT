97793c05f8032cdb26f3465a111107a3a035ab9c
beckie <beckiechebet6@gmail.com>
2019-10-11 12:52:01 +0300

    loopsandcomments

---
+++ new.js
@@ -0,0 +1,18 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+
+// var x = 1;
+// while(x<=10000)
+for (var x =1; x <= 100; x++)
+{
+    console.log(x);
+}
